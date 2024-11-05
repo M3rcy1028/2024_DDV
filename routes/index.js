d4c0295c1b04ec073c7d93a62110c867b0216e3a
M3rcy1028 <naryun1028@gmail.com>
2024-11-05 19:06:20 +0900

    아이디찾기 사이트 생성 및 데이터베이스 연결

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    connectionLimit: 5,
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'tutorial'
+});
 
 //시작 화면
 router.get('/', function (req, res, next) {
@@ -7,13 +15,58 @@ router.get('/', function (req, res, next) {
 });
 
 //회원가입 화면
+router.post('/joinForm', function (req, res, next) {
+  var Pdatas = [
+    req.body.id,
+    req.body.Lname,
+    req.body.Fname,
+    req.body.Bdate,
+    req.body.sex,
+    req.body.email
+  ];
+
+  var Udatas = [
+    req.body.id,
+    req.body.passwd,
+    req.body.nickname,
+  ];
+
+  var sql1 = "INSERT INTO PERSON(Pid, Lname, Fname, Bdate, Sex, Email) VALUES(?,?,?,?,?,?);";
+  connection.query(sql1, Pdatas, function(err, rows) {
+    if (err) {
+      console.error("err: " + err);
+      return res.status(500).send("데이터베이스 오류 발생");
+    }
+    console.log("rows: " + JSON.stringify(rows));
+  });
+  var sql2 = "INSERT INTO USR(Uid, Pwd, Nickname) VALUES(?,?,?);";
+  connection.query(sql2, Udatas, function(err, rows) {
+    if (err) {
+      console.error("err: " + err);
+      return res.status(500).send("데이터베이스 오류 발생");
+    }
+    console.log("rows: " + JSON.stringify(rows));
+  });
+  res.redirect('/login'); // 회원가입 후 리다이렉트
+});
+
 router.get('/joinForm', function (req, res, next) {
   res.render('joinForm', { title: '회원가입' });
+})
+
+//관리자 로그인 화면
+router.get('/rootLogin', function (req, res, next) {
+  res.render('rootLogin', { title: '관리자 로그인' });
 })
 
 //로그인 화면
 router.get('/login', function (req, res, next) {
   res.render('login', { title: '로그인' });
+})
+
+//아이디 찾기 화면
+router.get('/findId', function (req, res, next) {
+  res.render('findId', { title: '아이디 찾기' });
 })
 
 //비밀번호 찾기 화면
---
+++ routes/viewUsers.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+//글쓰기 조회 화면
+router.get('/sellList', function (req, res, next) {
+    res.render('sellList', { title: '판매 글보기' });
+})
+
+//글쓰기 조회 화면
+router.get('/sellRead', function (req, res, next) {
+    res.render('sellRead', { title: '판매 글보기' });
+})
+    
+//글쓰기 화면
+router.get('/sellWrite', function (req, res, next) {
+    res.render('sellWrite', { title: '판매 글쓰기' });
+})
+
+//글쓰기 수정 화면
+router.get('/sellUpdate', function (req, res, next) {
+    res.render('sellUpdate', { title: '판매 글 수정하기' });
+})
+
+module.exports = router;
