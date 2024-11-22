5bc2c7eb445576b2f325616ea1cfaabf8aecab72
Sonmin <handbear1208@gmail.com>
2024-11-23 00:28:05 +0900

    상단바 파일 생성

--- controllers/SellController/sellWriteController.js
+++ controllers/SellController/sellWriteController.js
@@ -2,7 +2,8 @@ var writeModel = require('../../models/SellModel/sellWriteModel');
 var express = require('express');
 
 exports.writeForm = (req, res) => {
-    res.render('SellFunction/sellWrite', { title: "상품 등록" });
+    var { rootLogin, usrLogin } = require('../../routes/index.js'); //사용자, 관리자 로그인 여부
+    res.render('SellFunction/sellWrite', { title: "상품 등록" , rootLogin, usrLogin});
 }
 
 exports.writeData = (req, res) => {
