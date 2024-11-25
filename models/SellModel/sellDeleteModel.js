f2ea8e0dd8f761ac40099e1a5e2e924d9ec2b950
KimTaegwan03 <aetaik97@kw.ac.kr>
2024-11-25 12:30:51 +0900

    chatroom complete

--- models/SellModel/sellDeleteModel.js
+++ models/SellModel/sellDeleteModel.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '1234',
-    database: 'tutorial'
+    database: 'ddv'
 });
 
 exports.deleteRow = (datas, callback) => {
